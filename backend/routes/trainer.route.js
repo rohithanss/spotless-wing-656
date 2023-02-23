1b0bebbe2cdd80f320f1e28bce06b0178c292e9a
BmDevelopers001 <bmsavaliya001@gmail.com>
2023-02-23 14:19:19 +0530

    responses constraints updates

--- backend/routes/trainer.route.js
+++ backend/routes/trainer.route.js
@@ -9,11 +9,11 @@ trainerRouter.post('/addslot', async (req,res) => {
 
     await bookings.create({ trainer_id, activity_type, fees, reg_date, six, seven, eight, nine, ten, eleven, twelve, one, two, three, four, five, six_eve, seven_eve, eight_eve })
     .then(() => {
-        res.status(200).send({'msg' : 'Bookings created successfully'})
+        res.status(200).send({status : 'success' ,msg : 'Bookings created successfully'})
     })
     .catch((err) => {
         console.error('Failed to add bookings :', err);
-        res.status(400).send({'err' : 'error creating bookings'})
+        res.status(400).send({status : 'error',err : 'error creating bookings'})
     })
 })
 
