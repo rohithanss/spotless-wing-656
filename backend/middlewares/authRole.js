98ad164e102efe26c8bf8a2d7db703fc85061f15
BmDevelopers001 <bmsavaliya001@gmail.com>
2023-02-24 12:38:11 +0530

    changes

--- backend/middlewares/authRole.js
+++ backend/middlewares/authRole.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const authRole = (roleArr) => {
     return (req,res,next) => {
-        const token = req.headers.token?.split(' ')[1];
+        const token = req.headers.authorization?.split(' ')[1];
 
         jwt.verify(token, process.env.PRIVATEKEY, (err, decoded) => {
             if (err) {
