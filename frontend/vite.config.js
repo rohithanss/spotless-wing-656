2c0f7237ee97d00769ccbcb223037fc413fecf8e
Rohit Hans <rhans6125@gmail.com>
2023-02-26 00:23:14 +0530

    Error(logo name) fixing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        "node:*",
+        "images/abc.png", // i.e. this syntax does not work, is there something similar that would work?
+      ],
+    },
+  },
+});
