581249342167c9a9bc78b7127fe6218c9a8d8392
Rohit Hans <rhans6125@gmail.com>
2023-02-26 13:06:01 +0530

    Backend Deployed

--- frontend/src/scripts/api.js
+++ frontend/src/scripts/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:8000";
+const url = "https://brohfitness.onrender.com";
 
 async function signup(name, email, phone, password, role) {
   try {
